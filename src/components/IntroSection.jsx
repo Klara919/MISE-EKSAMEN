ada8ed9609b44dd626f79bf31319bb53905502b6
Klara919 <eaa25Kmbj@students.eaaa.dk>
2026-04-08 10:03:38 +0200

    Update IntroSection.jsx

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./IntroSection.css";
-import Team from "../assets/team.jpg";
+import Team from "../assets/Team.jpg";
 
 export default function IntroSection() {
   return (
